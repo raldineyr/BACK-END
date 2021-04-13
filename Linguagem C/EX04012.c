351426e9f3e40241154a091bdbc82acaacc6e85d
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-13 06:49:28 -0300

    Exercicio do capitulo 04, número 12.

---
+++ Linguagem C/EX04012.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Hickory, dickory, dock The mouse ran up the clock The clock struck one, The mouse ran down, Hickory, dickory, dock.");
+return (0);
+}
