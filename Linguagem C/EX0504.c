195b92ce5712d6e779f4a9957ba33f13f23ab458
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-14 06:51:18 -0300

    Exercício do capítulo 05, número 04.

---
+++ Linguagem C/EX0504.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    puts("Valores 8 e 2:");
+    printf("Adicao e %d\n",2+8);
+    printf("Subtracao e %d\n",8-2);
+    printf("Multiplicacao e %d\n",8*2);
+    printf("Divisao e %d\n",8/2);
+
+    return (0);
+}
