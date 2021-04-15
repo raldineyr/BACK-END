795a63c676d932ee0c31333c1b6e967451ce01ab
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-15 06:12:43 -0300

    Exercício do capítulo 05, número 11.

---
+++ Linguagem C/EX0511.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("O valor e: %d\n",3);
+    printf("E %d e o valor\n",3);
+    printf("Nao e %d\n",3+1);
+    printf("Nao e %d\n",3-1);
+    printf("Nao, o valor e %d\n",3);
+    return 0;
+}
