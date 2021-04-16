c78617bd161d45d097268457416eedea0e1ded9f
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-16 05:53:21 -0300

    Exercício do capítulo 05, número 14.

---
+++ Linguagem C/EX0514.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#define VALOR 8
+
+int main()
+{
+    printf("O valor e: %d\n",VALOR);
+    printf("E %d e o valor\n",VALOR);
+    printf("E nao e: %d\n",VALOR + 1);
+    printf("E nao e: %d\n",VALOR - 1);
+    printf("Nao, o valor e: %d\n",VALOR);
+    return(0);
+}
