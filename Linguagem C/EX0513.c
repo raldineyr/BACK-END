618a929d3b59c903e86fd55dca70f474c7d36088
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-16 05:52:20 -0300

    Exercício do capítulo 05, número 13.

---
+++ Linguagem C/EX0513.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#define VALOR 3
+
+int main()
+
+{
+    printf("O valor e: %d\n", VALOR);
+    printf("E o valor e: %d\n", VALOR);
+    printf("Nao e: %d\n", VALOR + 1);
+    printf("Nao e: %d\n", VALOR - 1);
+    printf("Nao, o valor e: %d\n", VALOR);
+    return(0);
+}
