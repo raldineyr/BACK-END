bd9be87194b79fe8004062bbe5b090333b5143b6
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-17 08:12:14 -0300

    Exercício do capítulo 06, número 05.

---
+++ Linguagem C/EX0605.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define GLORKUS 16
+
+int main()
+{
+    char brief;
+
+    brief = 22;
+
+    printf("O valor de brief e: %d\n",brief);
+    printf("A soma entre brief e GLORKUS e: %d\n", brief + GLORKUS);
+    printf("A multiplicacao entre brief e GLORKUS e: %d\n",brief * GLORKUS);
+    return(0);
+}
