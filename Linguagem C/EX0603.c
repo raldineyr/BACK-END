e7d94fe97b56778c140789c6b72651a0c6c339c8
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-17 08:06:42 -0300

    Exercício do capítulo 06, número 03.

---
+++ Linguagem C/EX0603.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    int i;
+    float f;
+    double d;
+
+    c = 'a';
+    i = 1;
+    f = 19.0;
+    d = 20000.009;
+
+    printf("%c\n%i\n%f\n%f\n",c,i,f,d);
+    return(0);
+}
