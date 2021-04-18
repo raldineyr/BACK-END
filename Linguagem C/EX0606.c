6705bffaf3a5eef16283ded205c810aa2a6d2aaa
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-18 00:07:53 -0300

    Exercício do capítulo 06, número 06.

---
+++ Linguagem C/EX0606.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#define GLORKUS 16
+
+int main()
+{
+    int GLORKUS;
+
+    GLORKUS = 16;
+
+    printf("O valor de GLORKUS e: %d\n", GLORKUS);
+    printf("O valor de GLORKUS adicionado a 22 e: %d\n", GLORKUS + 22);
+    printf("O valor de GLORKUS multiplicado por GLORKUS e: %d\n"), GLORKUS * GLORKUS;
+
+    return (0);
+}
