555711ce121ee2ab91ce1f8ae99989beab59ff4c
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-18 00:10:39 -0300

    Exercício do capítulo 06, número 08.

---
+++ Linguagem C/EX0608.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char Shadrach, Meshach, Abednego;
+
+    Shadrach = 1;
+    Meshach = 2;
+    Abednego = 3;
+
+    printf("Shadrach: %d\nMeshach: %d\nAbednego: %d\n",Shadrach,Meshach,Abednego);
+    return(0);
+}
