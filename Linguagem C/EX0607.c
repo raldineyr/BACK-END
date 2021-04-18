3eda89f86a250760073bbfd2c2a4405a7329fed4
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-18 00:08:59 -0300

    Exercício do capítulo 06, número 07.

---
+++ Linguagem C/EX0607.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    unsigned int ono;
+
+    ono = -10;
+
+    printf("O valor de ono e: %u\n",ono);
+    return(0);
+}
+
+/* MORAL DA HISTORIA SE A SUA VARIAVEL INTEIRA ARMAZENA NUMEROS NEGATIVOS,
+VOCE NAO PODE UTILIZAR UM TIPO DE VARIAVEL UNSIGNED.*/
