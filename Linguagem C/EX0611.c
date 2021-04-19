f06f628f4f464199edcb889741d4f882af22762c
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-19 05:47:13 -0300

    Exercício do capítulo 06, número 11.

---
+++ Linguagem C/EX0611.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int variando;
+
+    variando = 1;
+    printf("Shadrach: %d\n", variando);
+
+    variando = 2;
+    printf("Meshach: %d\n", variando);
+
+    variando = 3;
+    printf("Abednego: %d\n", variando);
+
+    return (0);
+}
