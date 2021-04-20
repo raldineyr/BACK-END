b3582031904d106b7b435fc59b0909ead0647157
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-20 05:49:23 -0300

    Exercício do capítulo 07, número 05.

---
+++ Linguagem C/EX0705.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int ch;
+
+    printf("Aperte enter: ");
+    getchar();
+
+    ch = 'H';
+    putchar(ch);
+    ch = 'i';
+    putchar(ch);
+    putchar('!');
+
+    return (0);
+}
