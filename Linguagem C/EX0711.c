5cbddc54a4ccb5ecb187daa0cd527642c9a56fd7
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-21 06:19:08 -0300

    Exercício do capítulo 07, número 11.

---
+++ Linguagem C/EX0711.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Programa para a destruição do mundo.\n");
+    char prompt[] = "Aperte ENTER para explodir.\n";
+    printf("%s",prompt);
+    getchar();
+    return (0);
+}
