547687908437fe54451f9509672995c135237caf
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-21 06:09:28 -0300

    Exercício do capítulo 07, número 06.

---
+++ Linguagem C/EX0706.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int ch;
+
+    printf("Aperte ENTER: ");
+    getchar();
+    ch = 'H';
+    putchar(ch);
+    ch = 'i';
+    putchar(ch);
+    putchar('!');
+    putchar('\n');
+               return (0);
+}
