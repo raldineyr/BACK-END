57fab3c04315b11f73d58bea1cd31b403b2eb25f
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-21 06:12:37 -0300

    Exercício do capítulo 07, número 08.

---
+++ Linguagem C/EX0708.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char a, b, c, d;
+
+    a = 'W';
+    b = 97 + 24;
+    c = 98 + 9;
+    d = '\n';
+
+    printf("%c%c%c%c", a, b, c, d);
+    return(0);
+}
