111be06d47d6ea12b98162fa77e4bda902f132e5
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-21 06:14:35 -0300

    Exercício do capítulo 07, número 09.

---
+++ Linguagem C/EX0709.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    char a, b, c, d;
+
+    a = 'W';
+    b = 'o';
+    c = 'w';
+    d = '\n';
+
+    putchar(a);
+    putchar(b);
+    putchar(c);
+    putchar(d);
+
+    return(0);
+}
