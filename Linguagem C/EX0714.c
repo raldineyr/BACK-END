44d939afc15a8cde8fe8367cdea17c0e7f53df76
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-22 06:22:28 -0300

    Exercício do capítulo 07, número 14.

---
+++ Linguagem C/EX0714.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int fav;
+        printf("Qual e o seu numero favorito: ");
+        scanf("%d",&fav);
+
+        printf("%d E o meu numero favorito tambem!",fav);
+
+    return (0);
+}
