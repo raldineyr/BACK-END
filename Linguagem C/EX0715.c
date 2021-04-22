0d6d439e6de3f8fa503935c57325f5d5aa9a5a5b
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-22 06:23:03 -0300

    Exercício do capítulo 07, número 15.

---
+++ Linguagem C/EX0715.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    float fav;
+
+    printf("Digite o seu nome favorito: ");
+    scanf("%f",&fav);
+
+    printf("%f tamb�m e o meu numero favorito!",fav);
+
+    return (0);
+}
