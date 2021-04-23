97a0b6d9f09ad533c8f18ff8794f05459f264348
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-23 05:53:21 -0300

    Exercício do capítulo 08, número 03.

---
+++ Linguagem C/EX0803.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    a = 5;
+    b = a + 10;
+
+    if (a < b)
+
+        printf("O valor de %d e menor que o valor de %d", a, b);
+
+    return (0);
+}
+
+// ERRANDO DE PROPOSITO.
