bc57041558f22ab1151350c57ed577cffbe7b93a
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-24 08:20:40 -0300

    Exercício do capítulo 08, número 08.

---
+++ Linguagem C/EX0808.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+
+    a = 5;
+
+    if (a == -3)
+    {
+        printf("%d e igual a %d", a, -3);
+    }
+    return(0);
+}
+
+// ERRANDO DE PROPOSITO
