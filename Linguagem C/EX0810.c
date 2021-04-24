cdd0d629d941eb60a69809ed2cbb4e94f8daaa01
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-24 08:21:51 -0300

    Exercício do capítulo 08, número 10.

---
+++ Linguagem C/EX0810.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+
+    a = 6;
+    b = a - 2;
+
+    if(a > b)
+    {
+        printf("%d e maior que %d", a, b);
+    }
+    else
+    {
+        printf("%d e menor que %d", a, b);
+    }
+    return (0);
+}
