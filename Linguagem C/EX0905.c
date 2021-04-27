71b9dc8c77239f70aadb1567fc3799d5ed2ca792
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-27 07:05:32 -0300

    Exercício do capítulo 09, número 05.

---
+++ Linguagem C/EX0905.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int duplo;
+
+    for(duplo=2; duplo<=100; duplo=duplo+2)
+    {
+        printf("%d\t",duplo);
+    }
+    putchar('\n');
+    return (0);
+}
