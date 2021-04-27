610d2d2e64f5af17c99cea051bdd2734ea2d0954
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-27 07:03:09 -0300

    Exercício do capítulo 09, número 03.

---
+++ Linguagem C/EX0903.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int contagem;
+
+    for(contagem = -5; contagem < 6; contagem = contagem +1)
+    {
+        printf("%d\n", contagem);
+    }
+    return(0);
+}
