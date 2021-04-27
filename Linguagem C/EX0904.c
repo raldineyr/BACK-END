859139f8adbbf1a6fbd8be5ac83a91acca66528d
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-27 07:04:21 -0300

    Exercício do capítulo 09, número 04.

---
+++ Linguagem C/EX0904.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int contagem;
+
+    for (contagem = 11; contagem <= 19; contagem = contagem +1)
+    {
+        printf("%d\t",contagem);
+    }
+    return(0);
+}
