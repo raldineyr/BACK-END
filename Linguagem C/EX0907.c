8d8ed2dc814beb35d1c83487bbe62c3cfb5aa4bb
raldineyr <64384382+raldineyr@users.noreply.github.com>
2021-04-27 07:07:19 -0300

    Exercício do capítulo 09, número 07.

---
+++ Linguagem C/EX0907.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int regressiva;
+
+    for (regressiva = 25; regressiva >=0; regressiva = regressiva-1)
+    {
+        printf("%d\t",regressiva);
+    }
+    return(0);
+}
