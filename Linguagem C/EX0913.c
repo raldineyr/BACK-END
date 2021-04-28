cb2731e7422473098ad0b379fbf7ea3229fe794b
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-04-28 06:20:49 -0300

    Exercício do capítulo 09, número 13.

---
+++ Linguagem C/EX0913.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main ()
+
+{
+    int x;
+
+    x = 0;
+    while (x < 10)
+    {
+        puts("Eu sinto muito, cirurgia de ombro.");
+        x = x+1;
+    }
+    return (0);
+}
