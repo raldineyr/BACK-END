ea63a8901059c1849197a3d29c92cead9a38479c
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-04-29 07:03:49 -0300

    Exercício do capítulo 09, número 17.

---
+++ Linguagem C/EX0917.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    x = 13;
+
+    do
+    {
+        printf("Sinto muito, cirurgia de ombro.");
+        x = x+1;
+    }while(x < 10);
+    return(0);
+}
