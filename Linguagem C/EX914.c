ffc8d5c72d2ff72f5cdeae58ec9832300030ed29
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-04-29 07:00:40 -0300

    Exercício do capítulo 09, número 14.

---
+++ Linguagem C/EX914.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    x = 13;
+    while (x < 10)
+    {
+        puts("Sinto muito, cirurgia de ombro.");
+        x = x + 1;
+    }
+    return(0);
+}
+// ERRANDO DE PROPOSITO
