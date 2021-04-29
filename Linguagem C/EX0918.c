a4e3066693fd091c9223419a2d808d838e248d80
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-04-29 07:06:38 -0300

    Exercício do capítulo 09, número 18.

---
+++ Linguagem C/EX0918.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    for (x = 0; x = 10; x = x + 1)
+    {
+        puts("O que voce esta olhando?");
+    }
+    return(0);
+}
+
+//EXERCICIO LOOP INIFINITO
