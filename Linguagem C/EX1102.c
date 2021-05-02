1d8efe0150dbadd1b4836edc48c3dc2f6c8fa5d8
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-05-02 10:15:30 -0300

    Exercício do capítulo 11, número 02.

---
+++ Linguagem C/EX1102.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+
+    x = 0;
+
+    while(x < 100)
+    {
+        printf("Saiam do meu gramado, seus pirralhos\n");
+        x++;
+    }
+
+    return (0);
+}
