f5118e1a8405289fab57178a95cef2ee7edc6ff3
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-05-04 07:27:42 -0300

    Exercício do capítulo 11, número 18.

---
+++ Linguagem C/EX1118.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int r,a,b;
+
+    puts("100 Numeros aleatorios: ");
+    for(a = 0; a < 20; a++)
+    {
+        for (b = 0; b < 5; b++)
+        {
+
+            r = rand();
+            r % 21;
+            printf("%d\t",r);
+        }
+        putchar('\n');
+    }
+    return 0;
+}
