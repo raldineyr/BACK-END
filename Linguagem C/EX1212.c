cdb4dbfd770a7b4504068fcd6c496f8a9fb7214e
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-05-10 05:32:58 -0300

    Exercício do capítulo 12, número 12.

---
+++ Linguagem C/EX1212.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    char nome, sobrenome;
+
+    printf("Informe o seu nome: ");
+    scanf("%15s %15s",nome, sobrenome);
+
+    printf("Ola %s %s e um prazer te conhecer.");
+    return 0;
+}
