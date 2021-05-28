e6040427aff81cedd3fe208f6f0b8b3123deb686
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-05-28 08:33:38 -0300

    Exercício do capítulo 13, número 02.

---
+++ Linguagem C/EXE1302.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+
+
+    char frase[]= "\n\nSOMETIMES\n\nAs vezes e um vento mais forte\ne ele vem de longe, tangendo as colinas\
+\nE as tardes se emancipam de mim,\ncomo se fossem feitas de puro desejo.\n\nUm azul intenso devora meus dedos\
+\ne os olhos, inteiros, sao de oceano e vao\ne eu estou perdida: nao ha portas\
+mas as chaves persistem,\npendendo de minhas maos.\n\nUm vento que me fala em uma outra lingua\
+\ne, ainda assim,\ntoda me devora,\ne nao ha apelo,\n\
+e nao ha distancia que o coloque de volta:\nentra pelos meus cabelos\ne faz deles sua mais perfeita morada.\n\n\
+Um vento, e eu de todo exilada.\nUm vento, e eu desfeita,\ncalada.\nUm vento e, pobre de mim,\nsou toda feita de agua.\n\n- Livia Natalia\n\n";
+
+    int indice, alpha, blank, punct, upper, lower;
+
+    alpha = blank = punct = upper = lower = 0;
+
+    indice = 0;
+
+    while (frase[indice]){
+
+        if(isalpha(frase[indice]))
+            alpha++;
+
+        if(isblank(frase[indice]))
+            blank++;
+
+        if(ispunct(frase[indice]))
+            punct++;
+
+        if (islower(frase[indice]))
+            lower++;
+
+        if (isupper(frase[indice]))
+            upper++;
+
+
+        indice++;
+    }
+
+    printf("\"%s\"\n",frase);
+
+    puts("Estatistica: \n");
+
+    printf("%d caracteres do alfabeto;\n",alpha);
+    printf("%d caracteres em branco ou nulo;\n",blank);
+    printf("%d simbolos de pontuacao;\n",punct);
+    printf("%d Caracteres minusculos;\n",lower);
+    printf("%d Caracteres maiusculo;\n",upper);
+    printf("%d Caracteres totais.\n",indice);
+
+
+    return 0;
+}
