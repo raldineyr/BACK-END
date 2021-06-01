6f0f361604fd07345ec7a5063e1b9510e854e288
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-06-01 08:42:39 -0300

    Exercício 02

---
+++ Linguagem Java/BosonTreinamentos/OperadoresAtribuicao.java
@@ -0,0 +1,12 @@
+package BosonTreinamentos;
+
+public class OperadoresAtribuicao{
+	public static void main(String[] args) {
+		
+		int numero = 0;
+		
+		System.out.println("Valor Original: " + numero);
+		
+		numero += 3;
+	}
+}
