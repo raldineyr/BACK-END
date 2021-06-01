5848dcc30fa1c4c175f0cd48f4505a27c952700a
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-06-01 08:44:17 -0300

    Exercício 04

---
+++ Linguagem Java/BosonTreinamentos/DeclaracaoAtribuicao.java
@@ -0,0 +1,22 @@
+package BosonTreinamentos;
+
+public class DeclaracaoAtribuicao {
+	
+	public static void main(String[] args) {
+		
+		boolean VouF = true;
+		byte a;
+		char letra = 'F';
+		int valor = 4500;
+		double pi = 3.14;
+		a = 20;
+		
+		System.out.printf("Valor de V ou F (BOOLEAN): %b\n",VouF);
+		System.out.printf("Valor de a (BYTE): %dn",a);
+		System.out.printf("Valor de letra (CHAR): %c\n",letra);
+		System.out.printf("Valor de valor (INT): %d\n",valor);
+		System.out.printf("Valor de pi (DOUBLE): %2.2f",pi);
+			
+	}
+
+}
