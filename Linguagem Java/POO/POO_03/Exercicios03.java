bb6557fd1f3112222e3cdd32bb20d5c43473c85d
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-06-15 07:50:30 -0300

    Exercício 03

---
+++ Linguagem Java/POO/POO_03/Exercicios03.java
@@ -0,0 +1,14 @@
+package Exercicios03;
+
+public class Principal {
+	
+	public static void main (String[] args) {
+		
+		Livro l1 = new Livro();
+		
+		l1.setPagina(0);
+		l1.status();
+		l1.folheando();	
+	}
+
+}
