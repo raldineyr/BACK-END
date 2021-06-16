4e1d5014b729067826a6479ef54085dd2445981c
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-06-16 08:27:17 -0300

    Exercício 04

---
+++ Linguagem Java/POO/POO_04/Principal.java
@@ -0,0 +1,20 @@
+package POO04;
+
+public class Principal {
+	
+	public static void main (String[] args) {
+		
+		Caneta c1 = new Caneta();
+		
+		c1.setModelo("BIC"); /// Método acessor.
+	    // c1.modelo = "BIC";  Acesso direto pelo atributo.
+		
+		c1.setPonta(0.5f);
+		// c1.ponta = 0.5f; Acesso direto pelo atributo interrompido devido ao método privado do atributo,
+		
+ 		c1.status();
+ 		
+ 		System.out.println("Tenho uma caneta " + c1.getModelo() + " de ponta " + c1.getPonta());
+	}
+
+}
