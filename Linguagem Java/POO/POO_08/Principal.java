526266dfbf04d4a24c2266e3e61f77dcba92e86d
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-06-27 18:28:34 -0300

    Exercício 08

---
+++ Linguagem Java/POO/POO_08/Principal.java
@@ -0,0 +1,10 @@
+package POO08;
+public class Principal {
+public static void main(String[] args){
+	
+	ControleRemoto controle = new ControleRemoto();	
+	
+	controle.ligar();
+	controle.abrirMenu();
+	}
+}
