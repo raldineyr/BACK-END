02f720e8375f71bdc2a23e96359b04bf8c597dc2
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-06-27 18:22:58 -0300

    Exercício 07

---
+++ Linguagem Java/POO/POO_07/Principal.java
@@ -0,0 +1,18 @@
+package POO07;
+
+public class Principal {
+	
+	public static void main (String[] args) {
+		
+		Caneta c1 = new Caneta("NIC","Amarela", 0.4f);
+		
+		c1.status();
+		
+		Caneta c2 = new Caneta ("Faber Castel","Vermelha", 0.7f);
+		
+		c2.status();
+		
+	}
+
+}
+
