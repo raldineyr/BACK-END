d01f6ad0a981f7a1f793fced1c1f51085d4e2a12
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-06-30 10:37:14 -0300

    Exercício 02

---
+++ Linguagem Java/POO/POO_02/Caneta.java
@@ -0,0 +1,48 @@
+package POO02;
+
+public class Caneta {
+
+	// Atributos
+
+	public String modelo;
+	public String cor;
+	private float ponta;
+	protected int carga; 
+	private boolean tampa; 
+	
+	/* Se o atributo conter a visibilidade privada ele tornasse
+	indisponivel para alterações. */ 
+
+	// métodos
+
+	void status() {
+
+		System.out.println("Modelo: " + this.modelo);
+		System.out.println("\nUma caneta " + this.cor);
+		System.out.println("\nPonta: " + this.ponta);
+		System.out.println("\nCarga: " + this.carga + "%");
+		System.out.print("\nUma caneta " + this.cor);
+		System.out.println("\nCom tampa: " + this.tampa + "\n");
+
+	}
+
+	public void rabiscar() {
+		
+		if (this.tampa == true) System.out.println("ERRO! Não posso rabiscar.");	
+		
+		else System.out.println ("Estou rabiscando!");			
+	}
+
+	/* Manobra para utilizar um atributo privado. */	
+	
+	public void tampada() { 
+
+		this.tampa = true;
+	}
+
+	public void destampada() {
+
+		this.tampa = false;
+	}
+
+}
