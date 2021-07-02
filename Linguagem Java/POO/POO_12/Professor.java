cb768dbfeb5dabc9208a7c8cec48a5c830e919e2
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-02 06:15:41 -0300

    Exercício 12

---
+++ Linguagem Java/POO/POO_12/Professor.java
@@ -0,0 +1,31 @@
+package POO12;
+
+public class  extends Pessoa{
+	
+	// Atributos
+	
+	private String especialidade;
+	private float salario;
+	
+	// Métodos acessores e modificadores
+	
+	public String getEspecialidade(){
+		
+		return this.especialidade;
+	}
+	
+	public void setEspecialidade(String especialidade){
+		
+		this.especialidade = especialidade;
+	}
+	
+	public float getSalario(){
+		
+		return this.salario;
+	}
+	
+	public void setSalario(float salario){
+		this.salario = salario;
+	}
+
+}
