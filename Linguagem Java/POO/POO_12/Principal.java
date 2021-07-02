90dd73ade47906017c9ad20e9ec13680f6cbeaa8
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-02 06:15:10 -0300

    Exercício 12

---
+++ Linguagem Java/POO/POO_12/Principal.java
@@ -0,0 +1,28 @@
+package POO12;
+
+public class {
+	public void main(String[] args) {
+		
+		Pessoa p1 = new Aluno();
+		Aluno p2 = new Aluno();
+		Professor p3 = new Professor();
+		Funcionario p4 = new Funcionario();
+		
+		p1.setNome("Raldiney");
+		p2.setNome("Maria");
+		p3.setNome("Marla");
+		p4.setNome("Safya Oluwafemi");
+		
+		p1.setGenero("M");
+		p2.setGenero("F");
+		p3.setGenero("F");
+		p4.setGenero("F");
+		
+		p1.setIdade(88);
+		p2.setIdade(55);
+		p3.setIdade(75);
+		p4.setIdade(24);
+		
+	}
+
+}
