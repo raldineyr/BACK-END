0524fd33f575451dee58931af9fcfc6987a3e739
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-05 08:34:27 -0300

    Exercícip 15

---
+++ Linguagem Java/POO/POO_15/Animal.java
@@ -0,0 +1,16 @@
+package POO15;
+
+public abstract class Animal {
+	
+	// Atributos animal
+	
+	protected boolean peso;
+	protected int idade;
+	protected int membros;
+	
+	// Métodos de animal 
+	
+	public abstract void locomover();
+	public abstract void alimentar();
+	public abstract void emitirSom();
+}
