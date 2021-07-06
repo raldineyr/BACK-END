5ae9101f621a728aaa64bccfd3b6d66564aa7bae
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-06 07:57:09 -0300

    Exercício 15

---
+++ Linguagem Java/POO/POO_15/Cobra.java
@@ -0,0 +1,15 @@
+package POO15;
+
+public class Cobra extends Reptil{
+
+	@Override
+	public void emitirSom(){
+		System.out.println("Sibilando");
+	}
+	
+	@Override 
+	public void alimentar(){
+		System.out.println("Comendo roedores");
+	}
+	
+}
