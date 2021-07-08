8472c92b7bb039033914f912317f7c4e9307d695
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-08 07:32:51 -0300

    Exercício 16

---
+++ Linguagem Java/POO/POO_16/Principal.java
@@ -0,0 +1,13 @@
+package POO16;
+
+public class Principal {
+	public static void main(String[] args){
+		
+		Cachorro cachorro = new Cachorro();
+		
+		cachorro.reagir(4,10);
+		cachorro.reagir(false);
+		cachorro.reagir("Olá");
+	}
+
+}
