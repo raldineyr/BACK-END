fb7735dd89bff259cb72cf9e27f87d46f001566d
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-10 11:01:51 -0300

    Exercício 02
    Área da circuferência

---
+++ Linguagem Java/Cod3rcursos/Fundamentos/Fundamentos02.java
@@ -0,0 +1,13 @@
+package Fundamentos;
+public class Fundamento02 {
+	public static void main(String[] args){
+		
+	double raio = 2.00;
+	final double pi = 3.14159;
+	// pi = 3.14; A palavra final impossibilita a variação dos dados na variavel pi.
+	System.out.printf("O valor da area é: %.4f\n",raio*raio*pi);
+	double area = pi * raio * raio;
+	System.out.println("A área é: " + area+" m2.");
+	}
+
+}
