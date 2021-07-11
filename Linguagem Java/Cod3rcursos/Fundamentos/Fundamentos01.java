0cb777c6097b2c6dd253096388d59729ad52ea21
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-11 06:39:08 -0300

    Exercício 01 - Hello World!

--- Linguagem Java/Cod3rcursos/Fundamentos/Fundamentos01.java
+++ Linguagem Java/Cod3rcursos/Fundamentos/Fundamentos01.java
@@ -2,7 +2,6 @@ package Fundamentos;
 
 public class Fundamento01 {
 	public static void main(String[] args){
-		
 	System.out.println("Hello world!\n");	
 	System.out.println("Primeiro programa parte #02\n");
 	System.out.println("Primeiro programa parte #03\n");
