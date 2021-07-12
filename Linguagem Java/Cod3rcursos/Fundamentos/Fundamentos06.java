c4a1131973cc4dc2a84a9f2b201de5abee2b54bc
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-12 09:30:17 -0300

    Exercíciio 06 - Funções a partir do ponto
    É um operador binário (possui dois operandos, um do lado esquerdo e outro do lado direito). Normalmente chamado dot operator.
    
    O mais comum é que do lado esquerdo fica o objeto ao qual está se referenciando. Do lado direito está a mensagem que está passando para o objeto. O mais comum é ser a invocação de um método, mas também pode ser o acesso direto à uma variável do objeto. Então ele é o operador de acesso a membros de um objeto. Existe caso que o lado esquerdo não ser o objeto mas algum membro que dá acesso ao um objeto.
    
    Há casos em que o acesso não é feito no objeto e sim diretamente na classe.

---
+++ Linguagem Java/Cod3rcursos/Fundamentos/Fundamentos06.java
@@ -0,0 +1,26 @@
+package Fundamentos;
+// Funções a partir do ponto.
+public class Fundamento06 {
+	public static void main(String[] args){
+		
+		String s = "Bom dia x";
+		s = s.replace("x","senhor"); // .replace(); É uma função de substituição.
+		s = s.toUpperCase(); // toUpperCase(); Modifica os caracteres para a forma maiúscula.
+		s = s.concat("!!!"); // Adiciona o dado informado na variavel
+		System.out.println(s);
+		
+		// As funções a partir do ponto também podem ser utilizadas diretamente nas impressões: syso
+		System.out.println("raldiney r".toUpperCase());
+		
+		// Pode-se também armazenar o dado em uma variável 
+		String x = "marlene r".toUpperCase();
+		System.out.println(x);
+		// Esta modificação acima também pode ser utilizada em funções .replace() e muito mais!!!
+		String y = "Boa noite y".replace("y","Princesa").toUpperCase().concat("!!!");
+		System.out.println(y);
+		
+		// Tipos primitivos não tem o operador ponto ".".
+		
+	}
+
+}
