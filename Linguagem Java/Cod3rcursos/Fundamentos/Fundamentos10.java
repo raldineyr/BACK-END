cddb8672d22d6d7fd057125f8ea4622f9bdf2211
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-13 09:30:59 -0300

    Objeto vs Tipo primitivo

---
+++ Linguagem Java/Cod3rcursos/Fundamentos/Fundamentos10.java
@@ -0,0 +1,14 @@
+package Fundamentos;
+public class Fundamento10 {
+	public static void main (String[] args){
+		
+		//String s = texto;
+		String s = new String("Texto");
+		s.toUpperCase();
+		
+		// Os tipos primitivos não tem comportamentos
+		int a = 123;
+		System.out.println(a);
+		// Wrappers são a versão objeto dos tipos primitivos
+	}
+}
