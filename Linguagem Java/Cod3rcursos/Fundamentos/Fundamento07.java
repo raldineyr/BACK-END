0c1cf1c6201d1cdb4cf78b62027063f4f4bdff68
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-13 09:24:12 -0300

    Exercício 07 - Import
    Import is a Java keyword. It declares a Java class to use in the code below the import statement.

---
+++ Linguagem Java/Cod3rcursos/Fundamentos/Fundamento07.java
@@ -0,0 +1,15 @@
+package Fundamentos;
+//Import
+import java.util.Date;
+
+public class Fundamento07 {
+	public static void main(String [] args) {
+		
+		String s = "Fora coiso!";
+		System.out.println(s);
+		
+		Date d = new Date();
+		System.out.println(d);
+
+	}
+}
