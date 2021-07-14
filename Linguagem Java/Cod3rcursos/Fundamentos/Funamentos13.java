b1208549f4751fbf9007429b4511bf84af28024f
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-14 05:07:26 -0300

    Exercício 13 - Conversão número para String

---
+++ Linguagem Java/Cod3rcursos/Fundamentos/Funamentos13.java
@@ -0,0 +1,14 @@
+package Fundamentos;
+// Conversão numero para String
+public class Fundamento13 {
+	public static void main(String[] args){
+		
+		
+		Integer numero1 = 10000;
+		System.out.println(numero1.toString().length());
+		
+		int numero2 = 100000;
+		System.out.println(Integer.toString(numero2).length());
+		
+	}
+}
