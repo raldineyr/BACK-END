ef9475c6ea6a264cbe361a5f526eb0c258a4d467
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-16 07:59:08 -0300

    Exercício 21 - Operadores de atribuição

---
+++ Linguagem Java/Cod3rcursos/Fundamentos/Fundamentos21.java
@@ -0,0 +1,21 @@
+package Fundamentos;
+//Operadores de atribuição
+
+public class Fundamento21 {
+	public static void main(String[] args){
+		
+		int a = 3;
+		int b = a;
+		int c = a + b;
+		
+		c++; // c = c + 1; Operador unário
+		
+		c += b; // c = c + b;
+		c -= a; // c = c - a;
+		c *= b; // c = c * b;
+		c /= a; // c = c / a;
+		c %= a; // c = c % a;
+
+	}
+
+}
