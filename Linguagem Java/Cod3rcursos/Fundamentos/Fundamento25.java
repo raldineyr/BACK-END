fc4bdf2b45fe49c57084e2c704a19def1c595384
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-17 07:41:05 -0300

    Exercício 25 - Desafio calculadora

---
+++ Linguagem Java/Cod3rcursos/Fundamentos/Fundamento25.java
@@ -0,0 +1,29 @@
+package Fundamentos;
+import java.util.Scanner;
+
+// Desafio calculadora
+public class Fundamento25 {
+
+	public static void main(String[] args){
+	Scanner entrada = new Scanner(System.in);
+	
+	System.out.printf("Digite o primeiro numero: ");
+	double numero1 = entrada.nextDouble();	
+	
+	System.out.printf("Digite o segundo numero: ");
+	double numero2 = entrada.nextDouble();
+	
+	System.out.printf("Digite o operador: ");
+	String operador = entrada.next();
+	
+	double resultado = "+".equals(operador) ? numero1 + numero2 : 0;
+	resultado = "-".equals(operador) ? numero1 - numero2 : resultado;
+	resultado = "*".equals(operador) ? numero1 * numero2 : resultado;
+	resultado = "/".equals(operador) ? numero1 / numero2 : resultado;
+	resultado = "%".equals(operador) ? numero1 % numero2 : resultado;
+			
+	System.out.printf("%.2f %s %.2f = %.2f",numero1,operador,numero2, resultado);
+		
+	entrada.close();
+ }
+}
