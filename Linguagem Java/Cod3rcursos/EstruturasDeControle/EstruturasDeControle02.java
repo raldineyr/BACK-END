dae0acd28c389655bdef3a3324b90511f427dc23
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-18 00:27:47 -0300

    Exercício 02 -  Desafio estrutura de controle: if

---
+++ Linguagem Java/Cod3rcursos/EstruturasDeControle/EstruturasDeControle02.java
@@ -0,0 +1,15 @@
+package EstruturasDeControle;
+
+public class EstruturasDeControle02 {
+	public static void main(String[] args) {
+
+		double nota = 1.3;
+
+		if (nota >= 9.0); { // Desafio: O ponto e vírgula desvincula as impressões da condição
+
+			System.out.println("Quadro de honra");
+			System.out.println("Você é fera!!!");
+		}
+
+	}
+}
