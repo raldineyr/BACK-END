1f4d0061d73ac8d95da51d87c67082f7838a2294
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-19 07:24:17 -0300

    Exercício 05 -  Estrutura de controle: While

---
+++ Linguagem Java/Cod3rcursos/EstruturasDeControle/EstruturasDeControle05.java
@@ -0,0 +1,17 @@
+package EstruturasDeControle;
+
+// Estrutura de controle While
+public class EstruturasDeControle05 {
+	public static void main(String[] args){
+		
+		int contador = 1;
+		
+		while(contador <= 10) {
+			
+			System.out.printf("contador = %d\n",contador);
+			contador++;
+		}
+		
+	}
+
+}
