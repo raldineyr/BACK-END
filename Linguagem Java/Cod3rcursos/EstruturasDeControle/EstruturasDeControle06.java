2b332a5c068f90c448d16d040af90ce8111196e1
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-19 07:26:38 -0300

    Exercício 06 - Estrura de controle: for

---
+++ Linguagem Java/Cod3rcursos/EstruturasDeControle/EstruturasDeControle06.java
@@ -0,0 +1,16 @@
+package EstruturasDeControle;
+// Estrura de controle for
+
+public class EstruturasDeControle06 {
+	
+	public static void main(String[] args){
+		
+		for(int contador = 0; contador <= 10; contador++) {
+			
+			System.out.printf("contador = %d\n",contador);
+		}
+		
+		
+	}
+
+}
