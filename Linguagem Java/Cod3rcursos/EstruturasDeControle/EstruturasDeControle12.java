cd79ba2f04cc048a1c87849d858398b8cbd060d2
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-20 05:50:06 -0300

    Exercício 12 - Loop for escada

---
+++ Linguagem Java/Cod3rcursos/EstruturasDeControle/EstruturasDeControle12.java
@@ -0,0 +1,15 @@
+package EstruturasDeControle;
+// Loop for
+
+public class EstruturasDeControle12 {
+	
+	public static void main(String[] args){
+		
+		String valor = "#";
+		for(int i = 1; i <= 5; i++) {
+			System.out.println(valor);
+			
+			valor += "#";
+		}
+	}
+}
