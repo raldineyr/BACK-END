7df60e055f0ef86ac9a46c7ea043de1781339443
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-20 05:46:59 -0300

    Exercício 10 - Loop For decrescente

---
+++ Linguagem Java/Cod3rcursos/EstruturasDeControle/EstruturasDeControle10.java
@@ -0,0 +1,15 @@
+package EstruturasDeControle;
+// Loop for decrescente
+public class EstruturasDeControle10 {
+	
+	public static void main(String[] args) {
+		
+		
+		for(int contador = 10; contador >= 0; contador -= 2 ){
+			
+			System.out.printf("Contador: %d\n", contador);
+		}
+		
+	}
+
+}
