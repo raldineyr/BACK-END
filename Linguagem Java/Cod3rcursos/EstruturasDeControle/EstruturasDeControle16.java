85e47421ea4c0195cfb93873b85bfb79812e8655
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-21 08:29:59 -0300

    Exercício 16 - Loop aninhado com Break;

---
+++ Linguagem Java/Cod3rcursos/EstruturasDeControle/EstruturasDeControle16.java
@@ -0,0 +1,16 @@
+package EstruturasDeControle;
+// Loop for aninhado com "break;"
+public class EstruturasDeControle16 {
+	public static void main(String[] args){
+		
+		for (int i = 0; i < 10; i++){
+			
+			if (i == 5){
+			break;	
+        
+			}
+			System.out.println(i);
+		}
+		System.out.println("Fim.");
+	}
+}
