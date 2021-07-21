9d4bf8f9a539dca7ca49a7e56f4d9011933328d5
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-21 08:23:23 -0300

    Exercício 13 - Desafio: Loop For

---
+++ Linguagem Java/Cod3rcursos/EstruturasDeControle/EstruturasDeControle13.java
@@ -0,0 +1,15 @@
+package EstruturasDeControle;
+// Desafio loop for
+public class EstruturasDeControle13 {
+	
+	public static void main(String[] args){
+		
+		String valor = "#";
+		
+		for (char i = 'A'; i <= 'E'; i++){
+			System.out.println(valor);
+			
+			valor += "#";	
+		}
+	}
+}
