aedb3f177f02bc4615f8080f6285de0e8a8d2e24
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-22 05:54:40 -0300

    Exercício 19 - Not recommended: Break like Go to.

---
+++ Linguagem Java/Cod3rcursos/EstruturasDeControle/EstruturasDeControle19.java
@@ -0,0 +1,19 @@
+package EstruturasDeControle;
+// Break não recomendado
+public class EstruturasDeControle19 {
+	
+	public static void main(String[] args){
+		
+	externo: for (int i = 0; i < 3; i++){
+			for (int j = 0; j < 3; j++){
+			
+				if (i == 1){
+				break externo;
+				}
+				System.out.printf("%d %d ", i, j);
+			}
+			System.out.println();
+		}
+		System.out.println("Fim!");
+	}
+}
