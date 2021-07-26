1bb0ce5400344ec57af6fcd02c2620e49cec1ea7
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-26 07:25:51 -0300

    Exercício 06 - Método construtor

---
+++ Linguagem Java/Cod3rcursos/ClassesEmetodos/ClassesEmetodos06/DataTeste.java
@@ -0,0 +1,23 @@
+package ClassesEmetodos06;
+//Desafio método construtor
+
+public class DataTeste {
+	
+	public static void main(String[] args){
+		
+		Data d1 = new Data(16,11,1971);
+		
+		var d2 = new Data();
+		
+		d2.dia = 01;
+		d2.mes = 01;
+		d2.ano = 1970;
+		
+		String dataFormatada1 = d1.obterDataFormatada();
+		System.out.println(dataFormatada1);
+		
+		String dataFormatada2 = d2.obterDataFormatada();
+		System.out.println(dataFormatada2);
+		
+	}
+}
