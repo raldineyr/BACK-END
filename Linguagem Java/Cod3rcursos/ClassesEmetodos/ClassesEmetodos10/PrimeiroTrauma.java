81792c86b62c9655045c33c3cad3955e4c9c4d6e
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-27 08:25:02 -0300

    Exercicio 10 - Desafio primeiro trauma

---
+++ Linguagem Java/Cod3rcursos/ClassesEmetodos/ClassesEmetodos10/PrimeiroTrauma.java
@@ -0,0 +1,15 @@
+package ClassesEmetodos10;
+// Desafio primeiro trauma 
+public class PrimeiroTrauma {
+	
+	int a = 3;
+	static int b = 4;
+	
+	public static void main(String[] args) {
+		
+		PrimeiroTrauma p = new PrimeiroTrauma();
+		System.out.println(p.a); // A partir da instância podemos acessar um membro de classe. 
+		
+		System.out.println(b);
+	}
+}
