0d93ffc4a2b0c99760dbcf76c145ebcdf4f387d1
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-27 08:14:01 -0300

    Exercício 09 - Desafio método construtor

---
+++ Linguagem Java/Cod3rcursos/ClassesEmetodos/ClassesEmetodos09/Data.java
@@ -0,0 +1,30 @@
+package ClassesEmetodos09;
+// Desafio método construtor
+
+public class Data {
+	
+	// Atributos 
+	
+	int dia;
+	int mes;
+	int ano;
+	
+	Data(int diaInicial, int mesInicial, int anoInicial){
+		
+		dia = diaInicial;
+		mes = mesInicial;
+		ano = anoInicial;
+	}
+	
+	Data(){
+		
+		dia = 01;
+		mes = 01;
+		ano = 1970;	
+	}
+	
+	String obterDataFormatada() {
+		
+		return String.format("%d/%d/%d\n", dia, mes, ano);
+	}
+}
