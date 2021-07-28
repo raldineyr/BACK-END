62f077dda87b4ad5e11aa246ab30318e24fde0e4
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-28 05:16:08 -0300

    Exercício 13 - Desafio do módulo

---
+++ Linguagem Java/Cod3rcursos/ClassesEmetodos/ClassesEmetodos13/Comida.java
@@ -0,0 +1,13 @@
+package ClassesEmetodos13;
+//Desafio do módulo
+
+public class Comida {
+
+	String nome;
+	float peso;
+	
+	Comida(String nome, float peso){
+		this.nome = nome;
+		this.peso = peso;
+	}
+}
