f4f849a26468348216f296c09ec2c9506d2024af
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-29 07:56:00 -0300

    Exercício 02 - Array com loop for e for each

---
+++ Linguagem Java/Cod3rcursos/Array/Array02.java
@@ -0,0 +1,18 @@
+package Array;
+// Array
+
+public class Array02 {
+	public static void main(String[] argas) {
+		
+	double[] notas = {9.9, 8.8, 7.2, 9.4};
+
+	for( int i = 0; i < notas.length; i++){
+		
+		System.out.println(notas[i] + " ");
+}
+	for (double nota: notas) {
+		System.out.println(nota + " ");
+		
+		}		
+	}
+}
