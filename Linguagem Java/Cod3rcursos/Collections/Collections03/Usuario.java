f38ce696514781ad7a68e70a113c03959372d7a3
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-07-30 07:48:40 -0300

    Exercício 03 - Lista -> Usuario

---
+++ Linguagem Java/Cod3rcursos/Collections/Collections03/Usuario.java
@@ -0,0 +1,53 @@
+package Collections03;
+
+public class Usuario {
+
+	public String nome;
+
+	
+	Usuario(String nome){
+		this.nome = nome;
+	}
+
+	public String toString() {
+		return "Meu nome é " + this.nome + ".";
+	}
+
+	// Implementação HashCode a:
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+	
+	
+	/* 
+	  	// Equals mais simples
+	  	
+	  public boolean equals(Object obj) {
+		Usuario outroUsuario = (Usuario) obj;
+		return this.nome.equals(outroUsuario.nome);
+	  
+	 */
+}
