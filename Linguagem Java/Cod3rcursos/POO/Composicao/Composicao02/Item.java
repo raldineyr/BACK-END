6413fab4eff5d22553b7e9008bdabb36e9d97bb3
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-02 08:35:23 -0300

    Exercício 02 - Relacionamento um para muitos.

---
+++ Linguagem Java/Cod3rcursos/POO/Composicao/Composicao02/Item.java
@@ -0,0 +1,15 @@
+package POO.Composicao02;
+//POO Relacionamento 1 para N.
+
+public class Item {
+	String nome;
+	int quantidade;
+	double preco;
+	
+	Item(String nome, int quantidade, double preco){
+		
+		this.nome = nome;
+		this.quantidade = quantidade;
+		this.preco = preco;	
+	}
+}
