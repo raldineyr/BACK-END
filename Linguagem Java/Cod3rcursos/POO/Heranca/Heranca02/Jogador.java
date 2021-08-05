ced39105fdbdfad6c1ea3fbeb735e6ac5263e538
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-05 08:06:20 -0300

    Exercício 02 - Herança e a Notação .super()

---
+++ Linguagem Java/Cod3rcursos/POO/Heranca/Heranca02/Jogador.java
@@ -0,0 +1,52 @@
+package POO.Heranca02;
+//Herança notação .super 
+
+public class Jogador {
+
+	int x;
+	int y;
+	int vida = 100;
+	
+boolean atacar (Jogador oponente){
+		
+		int deltaX = Math.abs(x - oponente.x);
+		int deltaY = Math.abs(y - oponente.y);
+		
+		if (deltaX == 0 && deltaY == 1){
+			
+			oponente.vida -= 10;
+			return true;		
+		} else if (deltaX == 1 && deltaY  == 0) {
+			
+			oponente.vida -= 10;
+			return true;
+	} else {
+		
+		return false;
+	}
+		
+}
+	boolean andar(Direcao direcao){
+		
+		switch (direcao) {
+		
+		case NORTE:
+			y++;
+			break;
+			
+		case LESTE:
+			x++;
+			break;
+			
+		case SUL:
+			y--;
+			break;
+		
+		case OESTE:
+			x --;
+			break;
+		}
+		
+		return true;
+	}	
+}
