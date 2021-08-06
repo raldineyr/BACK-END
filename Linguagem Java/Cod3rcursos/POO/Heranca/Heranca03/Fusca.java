0be393cead60462ccc831e4bd6c2a3171b353c1e
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-06 08:11:22 -0300

    Exercício 03 - Desafio Herança #01: Carro

---
+++ Linguagem Java/Cod3rcursos/POO/Heranca/Heranca03/Fusca.java
@@ -0,0 +1,10 @@
+package POO.Heranca03;
+//Desafio Herança #01: Carro
+
+public class Fusca extends Carro{
+
+	@Override
+	public String toString() {
+		return "Fusca: " + velocidade + " KM/H "; 
+	}
+}
