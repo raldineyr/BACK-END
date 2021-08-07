300931c71b0a7ded5c40d3058e2cdc52330e57f0
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-07 07:54:30 -0300

    Exercício 01 - Relação entre herança e construtor.

---
+++ Linguagem Java/Cod3rcursos/POO/Heranca/Heranca04/Jogador.java
@@ -0,0 +1,61 @@
+package POO.Heranca04;
+//Relação herança e construtor
+
+public class Jogador {
+
+	int x;
+	int y;
+	int vida = 100;
+	
+	Jogador(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	Jogador(){
+		
+	}
+	
+	boolean atacar (Jogador oponente){
+		
+		int deltaX = Math.abs(x - oponente.x);
+		int deltaY = Math.abs(y - oponente.y);
+		
+		if (deltaX == 0 && deltaY == 1){
+			
+			oponente.vida -= 10;
+			return true;		
+		} else if (deltaX == 1 && deltaY  == 0) {
+			
+			oponente.vida -= 10;
+			return true;
+	} else {
+		
+		return false;
+	}
+		
+}
+	boolean andar(Direcao direcao){
+		
+		switch (direcao) {
+		
+		case NORTE:
+			y++;
+			break;
+			
+		case LESTE:
+			x++;
+			break;
+			
+		case SUL:
+			y--;
+			break;
+		
+		case OESTE:
+			x --;
+			break;
+		}
+		
+		return true;
+	}	
+}
