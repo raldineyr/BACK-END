a932337e5ccef07e36400a71f8c2184758a52bd5
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-08 12:43:07 -0300

    Exercício 05 - Desafio construtor.

---
+++ Linguagem Java/Cod3rcursos/POO/Heranca/Heranca05/Carro.java
@@ -0,0 +1,28 @@
+package POO.Heranca05;
+// Desafio construtor
+
+public class Carro {
+	
+	final int VELOCIDADE_MAXIMA;
+	int velocidadeAtual;
+	int delta = 5;
+	
+	Carro(int velocidadeMaxima){
+		VELOCIDADE_MAXIMA = velocidadeMaxima;
+	}
+
+	void acelerador(){
+		if (velocidadeAtual + delta > VELOCIDADE_MAXIMA) velocidadeAtual = VELOCIDADE_MAXIMA;
+		else velocidadeAtual += delta;
+	}
+	
+	void frear(){	
+		
+		if (velocidadeAtual >= 5) velocidadeAtual -= 5;
+		else velocidadeAtual = 0;
+	}
+	
+	public String toString(){
+		return "KM/H: " + velocidadeAtual;
+	}
+}
