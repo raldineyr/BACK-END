f305e658e026cbd55fee722440a2ce3994673e1b
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-08 12:45:19 -0300

    Exercício 05 - Desafio construtor

---
+++ Linguagem Java/Cod3rcursos/POO/Heranca/Heranca05/PainelDeControle.java
@@ -0,0 +1,15 @@
+package POO.Heranca05;
+// Desafio construtor
+
+public class PainelDeControle {
+
+	public static void main(String[] args) {
+		
+		Carro Tesla = new Tesla();
+		
+		Tesla.acelerador(); 
+
+		System.out.println(Tesla);
+	
+	}
+}
