7c6844f573a024ff07843c062e778c4f761a2d47
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-09 08:01:42 -0300

    Exercício 01 - Níveis de visibilidade

---
+++ Linguagem Java/Cod3rcursos/POO/ModificadoresDeAcesso/CasaA/Ana.java
@@ -0,0 +1,11 @@
+package POO.ModificadoresDeAcesso.CasaA;
+//Níveis de visibilidade
+
+public class Ana {
+
+	private String segredo = "...";
+	String facoDentroDeCasa = "..."; // default ou pacote
+	protected String formaDeFalar = "...";
+	public String todosSabem = "...";
+
+}
