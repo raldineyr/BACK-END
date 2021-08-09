1b63744a1e86a57af8cd5a89395321fa298b3e91
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-09 08:04:31 -0300

    Exercício 01 - Níveis de visibilidade

---
+++ Linguagem Java/Cod3rcursos/POO/ModificadoresDeAcesso/CasaB/Julia.java
@@ -0,0 +1,18 @@
+package POO.ModificadoresDeAcesso.CasaB;
+import POO.ModificadoresDeAcesso.CasaA.Ana;
+
+//Niveis de visibilidade
+
+public class Julia {
+	/*
+	void testeAcessos() {
+		
+		Ana sogra = new Ana();
+		
+		System.out.println(sogra.segredo);
+		System.out.println(sogra.formaDeFalar);
+		System.out.println(sogra.todosSabem); 
+		
+	}
+	*/
+}
