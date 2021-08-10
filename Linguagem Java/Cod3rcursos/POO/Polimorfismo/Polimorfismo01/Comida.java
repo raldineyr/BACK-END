9367748ddc9b18a8205fcc22b6b4e55af5d1b0c5
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-10 05:23:40 -0300

    Exercício 01 - Fundamentos polimorfismo

---
+++ Linguagem Java/Cod3rcursos/POO/Polimorfismo/Polimorfismo01/Comida.java
@@ -0,0 +1,19 @@
+package POO.Polimorfismo;
+	// Fundamentos polimorfismo
+public class Comida {
+	
+private float peso;
+	
+	public Comida(float peso){
+		setPeso(peso);
+	}
+
+	public float getPeso() {
+		return peso;
+	}
+
+	public void setPeso(float peso) {
+		if (peso >= 0) this.peso = peso;
+	}
+
+}
