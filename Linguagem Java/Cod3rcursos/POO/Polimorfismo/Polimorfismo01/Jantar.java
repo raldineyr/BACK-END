16a67ea988536522a1e9142aac6088f3feb1545b
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-10 05:25:37 -0300

    Exercício 01 - Fundamentos polimorfismo

---
+++ Linguagem Java/Cod3rcursos/POO/Polimorfismo/Polimorfismo01/Jantar.java
@@ -0,0 +1,24 @@
+package POO.Polimorfismo;
+//Fundamentos polimorfismo
+public class Jantar {
+	
+	public static void main(String[] args) {
+		
+		Pessoa convidado = new Pessoa (99.65F);
+		
+		Arroz porcao1 = new Arroz(0.2F);
+		Feijao porcao2 = new Feijao(0.1F);
+		
+		System.out.println(convidado.getPeso());
+		
+		convidado.comer(porcao1);
+		convidado.comer(porcao2);
+		
+		System.out.println(convidado.getPeso());
+		
+		Sorvete sobremesa = new Sorvete(0.4F);
+		
+		convidado.comer(sobremesa);
+		System.out.println(convidado.getPeso());
+	}
+}
