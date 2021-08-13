b39625a68de57302bcf19fb0ab258d6c63057729
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-13 08:20:03 -0300

    Exercício 01 - Fundamentos classe abstrata

---
+++ Linguagem Java/Cod3rcursos/POO/Abstrato/Cachorro.java
@@ -0,0 +1,16 @@
+package POO.Abstrato;
+
+public class Cachorro extends Mamifero{
+	
+	@Override
+	public String mover(){
+		
+		return "Usando as patas";
+	}
+	
+	@Override
+	public String mamar(){
+		
+		return "Leite canino";
+	}	
+}
