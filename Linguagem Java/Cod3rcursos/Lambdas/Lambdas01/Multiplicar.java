aa4738b1436248089e6c16a1beb769fc26d1248b
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-16 08:00:27 -0300

    Exercício 01 - Fundamentos de função lambda.

---
+++ Linguagem Java/Cod3rcursos/Lambdas/Lambdas01/Multiplicar.java
@@ -0,0 +1,10 @@
+package Lambdas.Lambdas01;
+//Fundamentos de função Lambda.
+
+public class Multiplicar implements Calculo{
+	
+	public double executar (double a, double b) {
+		
+		return a * b;
+	}
+}
