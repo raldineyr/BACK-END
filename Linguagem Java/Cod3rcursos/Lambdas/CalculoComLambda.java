647e31ba4ee094f454e5b0caea9f284e9a0b0667
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-17 10:39:28 -0300

    Exercício 03 - Functional Interface BinaryOperator

---
+++ Linguagem Java/Cod3rcursos/Lambdas/CalculoComLambda.java
@@ -0,0 +1,30 @@
+package lambdas.lambdas03;
+import java.util.function.BinaryOperator;
+//interfaces funcionais BinaryOperator
+
+public class CalculoComLambda {
+	
+	public static void main(String[] args) {
+		
+		// Adição
+		BinaryOperator<Double> lambda = ( x, y) -> { return x + y; };
+		System.out.println(lambda.apply( 2.0, 3.0));
+		
+		// Multiplicação
+		lambda = ( x, y) -> x * y; 
+		System.out.println(lambda.apply( 2.0, 3.0));
+		
+		// Adição
+		BinaryOperator<Integer> lambda2 = (x, y) -> { return x + y; };
+		System.out.println(lambda2.apply( 2 , 3));
+		
+		// Subtração
+		lambda2 = ( x, y) -> x - y; 
+		System.out.println(lambda2.apply(3, 2));
+		
+		// Divisão
+		lambda2 = (x, y) -> x / y;
+		System.out.println(lambda2.apply(10, 2));
+		
+	}
+}
