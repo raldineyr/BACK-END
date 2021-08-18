919e2789106565fb20afa13ec6cec8c10ea5c2db
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-18 08:01:19 -0300

    Exercício 05 - Funcional interface: Predicate

---
+++ Linguagem Java/Cod3rcursos/Lambdas/Lambdas05/Predicado.java
@@ -0,0 +1,14 @@
+package lambdas.lambdas05;
+import java.util.function.Predicate;
+// Funcional interface: Predicate
+
+public class Predicado {
+	public static void main(String[] args) {
+		
+		Predicate <Produto> isCaro = 
+				produto -> (produto.preco * (1 - produto.desconto)) >= 900.00;
+		
+		Produto produto = new Produto("Bicicleta", 1000.00, 0.10);
+		System.out.println(isCaro.test(produto));
+	}
+}
