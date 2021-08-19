767e14a041f1bf8a40eb56c70009f5a60b878845
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-19 08:06:39 -0300

    Create PredicadoComposicao.java

---
+++ Linguagem Java/Cod3rcursos/Lambdas/PredicadoComposicao.java
@@ -0,0 +1,16 @@
+package lambdas.lambdas08;
+import java.util.function.Predicate;
+// Predicate Functions 
+
+public class PredicadoComposicao {
+	public static void main (String[] args) {
+		
+	Predicate <Integer> isPar = numero -> numero % 2 == 0;
+	Predicate <Integer> isTresDigitos = 
+			numero -> numero >= 100 && numero <= 999;
+		
+		System.out.println(isPar.test(122)); // É par?
+		System.out.println(isPar.and(isTresDigitos).test(22)); // É par e tem três digítos?
+		System.out.println(isPar.or(isTresDigitos).test(235)); // É par ou tem três digitos?
+	}
+}
