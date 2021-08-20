72385b869ffc9e35dd6d447f28587994fcdcd68c
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-20 08:52:04 -0300

    Exercício 12 - Desafio operador binário.

---
+++ Linguagem Java/Cod3rcursos/Lambdas/Lambdas12/Produto.java
@@ -0,0 +1,17 @@
+package Lambdas.Lambdas12;
+// Desafio: Operador binário
+
+public class Produto { 
+ 
+	final String nome;
+	final double preco;
+	final double desconto;
+	
+	
+	public Produto(String nome, double preco, double desconto) {
+		
+		this.nome = nome;
+		this.preco = preco;
+		this.desconto = desconto;
+	}
+}
