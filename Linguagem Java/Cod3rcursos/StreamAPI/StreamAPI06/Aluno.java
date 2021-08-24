70f38fc0a3f11a41721449d7db238b29ad97a2b0
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-24 07:33:33 -0300

    Exercício 06 - Desafio StreamAPI

---
+++ Linguagem Java/Cod3rcursos/StreamAPI/StreamAPI06/Aluno.java
@@ -0,0 +1,16 @@
+package StreamAPI.StreamAPI06;
+//Desafio
+public class Aluno {
+	
+	final String nome;
+	final double nota;
+	final char genero;
+	
+	public Aluno(String nome, double nota, char genero){
+		
+	this.nome = nome;
+	this.nota = nota;
+	this.genero = genero;
+		
+	}
+}
