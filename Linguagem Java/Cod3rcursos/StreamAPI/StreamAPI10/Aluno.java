08abf6ae763539eea8ebe0f1715f1e907a35efc9
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-08-26 07:38:37 -0300

    Exercício 10 - Stream API metódo: match

---
+++ Linguagem Java/Cod3rcursos/StreamAPI/StreamAPI10/Aluno.java
@@ -0,0 +1,21 @@
+package StreamAPI.StreamAPI10;
+
+public class Aluno {
+	
+	final String nome;
+	final double nota;
+	final boolean comportamento;
+	
+	public Aluno(String nome, double nota) {
+	
+		this(nome, nota, true);
+		
+	}
+	
+	public Aluno(String nome, double nota, boolean comportamento){
+		
+		this.nome = nome;
+		this.nota = nota;
+		this.comportamento = comportamento;
+	}
+}
