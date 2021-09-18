6dfd69888de56e130620e4adf7fa51e89a1a827d
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-18 12:28:00 -0300

    Exercício 04 - Tratamento de exceção personalizada

---
+++ Linguagem Java/Cod3rcursos/TratamentoDeErros/ExcecaoPersonalizadaB/TesteValidacoes.java
@@ -0,0 +1,21 @@
+package TratamentoDeErros.ExcecaoPersonalizadaB;
+
+public class TesteValidacoes {
+	public static void main (String[] args){
+		
+		try {
+			Aluno aluno = new Aluno("adfga", 113);
+			
+			Validar.aluno(aluno);
+			Validar.aluno(null);
+			
+		} catch (StringVaziaException e) {
+			System.out.println(e.getMessage());
+		} catch (NumeroForaIntervaloException | IllegalArgumentException e) {
+			System.out.println(e.getMessage());
+		}
+		
+	 	
+		System.out.println("FIM!");		
+	}
+}
