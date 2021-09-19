e2694974f4fa57a8abbf9fee4c5515ba3a07e295
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-09-19 08:53:00 -0300

    Exercício 06 - Tratamento try catch getCause

---
+++ Linguagem Java/Cod3rcursos/TratamentoDeErros/TratamentoDeExcecao06/Aluno.java
@@ -0,0 +1,58 @@
+package TratamentoDeErros.TratamentoDeExcecao06;
+
+public class Aluno {
+	
+	public final String nome;
+	public final double nota;
+	public final boolean comportamento;
+	
+	public Aluno(String nome, double nota) {
+	
+		this(nome, nota, true);
+		
+	}
+	
+	public Aluno(String nome, double nota, boolean comportamento){
+		
+		this.nome = nome;
+		this.nota = nota;
+		this.comportamento = comportamento;
+	}
+	
+	public String toString(){
+		return nome + " tem a nota: " + nota;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (comportamento ? 1231 : 1237);
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(nota);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		if (comportamento != other.comportamento)
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (Double.doubleToLongBits(nota) != Double.doubleToLongBits(other.nota))
+			return false;
+		return true;
+	}
+}
