c1827f20310833f42a11165998326b41848ecea3
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-02 06:57:40 -0300

    Exercício 02 - Exemplo básico com generics

---
+++ Linguagem Java/Cod3rcursos/Generics/Exercicio02/CaixaTeste.java
@@ -0,0 +1,20 @@
+package Generics;
+
+public class CaixaTeste {
+	
+	public static void main(String[] args){
+		
+		Caixa<String> caixaA = new Caixa<>();
+		caixaA.guardar("Segredo!");
+		
+		String coisaA = caixaA.abrir();
+		System.out.println(coisaA);
+		
+		Caixa<Double> caixaB = new Caixa<>();
+		caixaB.guardar(3.14159);
+		
+		Double coisaB = caixaB.abrir();
+		System.out.println(coisaB);
+		
+	}
+}
