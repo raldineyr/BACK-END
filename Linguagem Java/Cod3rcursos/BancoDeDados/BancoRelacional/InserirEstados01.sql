fb288d436928d1b6ffebe5d296321c420cbc0ced
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-13 22:07:58 -0300

    Exercício DB

---
+++ Linguagem Java/Cod3rcursos/BancoDeDados/BancoRelacional/InserirEstados01.sql
@@ -0,0 +1,24 @@
+INSERT INTO estados
+    (nome, sigla, regiao, populacao)
+VALUES 
+('Bahia', 'BA', 'Nordeste', 15.34),
+('Ceará', 'CE', 'Nordeste', 9.02),
+('Distrito Federal', 'DF', 'Centro-Oeste',3.04),
+('Espirito Santo', 'ES', 'Sudeste', 4.02),
+('Goiás', 'GO', 'Centro-Oeste',6.78),
+('Maranhao','MA', 'Nordeste', 7.00),
+('Mato Grosso do Sul', 'MS', 'Centro-Oeste',2.71),
+('Minas Gerais', 'MG', 'Sudeste', 21.12),
+('Pará', 'PA', 'Norte', 8.36),
+('Paraíba', 'ṔB', 'Nordeste', 4.03),
+('Paraná', 'PR', 'Sul', 11.22),
+('Pernambuco','PE','Nordeste', 9.47),
+('Piauí', 'PI', 'Nordeste', 3.22),
+('Rio de Janeiro', 'RJ', 'Sudeste', 16.72),
+('Rio Grande do SUl', 'RS', 'Sul',11.32),
+('Rondônia', 'RO', 'Norte', 1.01),
+('Roraima', 'RR', 'Norte', 0.52),
+('Santa Catarina', 'SC', 'Sul', 7.01),
+('Sao Paulo', 'SP', 'Sudeste', 45.10),
+('Sergipe', 'SE', 'Nordeste', 2.90),
+('Tocantins', 'TB', 'Norte', 1.55);
