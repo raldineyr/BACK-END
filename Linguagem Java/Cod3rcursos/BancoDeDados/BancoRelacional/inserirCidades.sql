123f0498ae4c7b4a0833f91968cee057919bdedb
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-15 22:38:26 -0300

    Exercício 07 - Inserção de elementos na tabela

---
+++ Linguagem Java/Cod3rcursos/BancoDeDados/BancoRelacional/inserirCidades.sql
@@ -0,0 +1,39 @@
+select * from estados where id = 255
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Campinas', 795, 255)
+
+Select * from cidades
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES ('Niteroi', 133.9, 250)
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES (
+    'Caruaru',
+     920.6,
+    (select id from estados where sigla = 'PE')  
+)
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES(
+    'Salvador',
+     2.90,
+     (select id from estados where sigla = 'BA')
+)
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES(
+    'Simões Filho',
+    0.13,
+    (select id from estados where sigla = 'BA')
+)
+
+INSERT INTO cidades (nome, area, estado_id)
+VALUES(
+    'Fortaleza',
+     2.00,
+    (select id from estados where sigla = 'CE')
+)
+
+select * from cidades
