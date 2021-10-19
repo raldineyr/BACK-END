26b5a31f6e0f6a4cf60b014845b8997f837b16fe
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-18 22:14:26 -0300

    Exercício 12 - Explorando os tipos de Joins

---
+++ Linguagem Java/Cod3rcursos/BancoDeDados/BancoRelacional/explorandoTiposJoins.sql
@@ -0,0 +1,11 @@
+-- OBS: Aula realizada com o Workbench
+
+select * from prefeitos;
+select * from cidades;
+
+select * from cidades c inner join prefeitos p on c.id = p.cidade_id;
+select * from cidades c left outer join prefeitos p on c.id = p.cidade_id;
+select * from cidades c right join prefeitos p on c.id = p.cidade_id;
+select * from cidades c full join prefeitos p on c.id = p.cidade_id;
+
+select * from cidades.c left outer join prefeitos p on c.id = p.cidade_id 
