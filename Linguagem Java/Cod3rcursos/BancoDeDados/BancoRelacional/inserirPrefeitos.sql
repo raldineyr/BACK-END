c78a9a0400c3d92c9a13cf732631d987059feb86
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-18 22:07:09 -0300

    Exercício 10 - Inserir Prefeitos

---
+++ Linguagem Java/Cod3rcursos/BancoDeDados/BancoRelacional/inserirPrefeitos.sql
@@ -0,0 +1,22 @@
+select * from cidades 
+
+INSERT INTO prefeitos
+    (nome, cidade_id)
+VALUES
+    ('Bruno Reis',5),
+    ('Diogenes Tolentino',6),
+    ('José Sarto',7),
+    ('João dos venenos',null);
+
+    select * from prefeitos;
+
+    Insert into prefeitos
+        (nome, cidade_id)
+    VALUES
+        ('Mel Ribeiro', null);
+
+    Insert into prefeitos
+        (nome, cidade_id)
+    VALUES
+        ('Armando Golpes', 5); -- Relacionamento: Um para um.
+
