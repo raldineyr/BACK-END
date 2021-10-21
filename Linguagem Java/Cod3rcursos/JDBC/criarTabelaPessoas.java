a8f71ebb5bf4f20cc300042fadc01bfaff8fd0a2
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-21 17:54:30 -0300

    Exercício 04 - Criar tabelas

---
+++ Linguagem Java/Cod3rcursos/JDBC/criarTabelaPessoas.java
@@ -0,0 +1,27 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class criarTabelaPessoas {
+	
+	public static void main(String[] args) throws SQLException {
+		
+		Connection conexao = FabricaConexao.getConexao();
+		
+		String sql = "CREATE TABLE IF NOT EXISTS pessoas ("
+				+ "codigo INT AUTO_INCREMENT PRIMARY KEY,"
+				+ "nome VARCHAR(80) NOT NULL"
+				+ ")";
+		
+		Statement stmt = conexao.createStatement();
+		stmt.execute(sql);
+		
+		System.out.println("Tabela criada com sucesso!");
+		
+		conexao.close();
+		
+		
+	}
+}
