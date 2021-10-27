f72e2924bf0140a28c9240c856509df7ae3bb8b2
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-10-26 22:16:34 -0300

    Exercício 06 - Inserir registros

---
+++ Linguagem Java/Cod3rcursos/JDBC/ConsultarPessoa1.java
@@ -0,0 +1,37 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConsultarPessoa1 {
+	
+	public static void main(String[] args) throws SQLException{
+		
+		Connection conexao = FabricaConexao.getConexao();
+		String sql = "SELECT * FROM pessoas";
+		
+		Statement stmt = conexao.createStatement();
+		ResultSet resultado = stmt.executeQuery(sql);
+		
+		List<Pessoa> pessoas = new ArrayList<>();
+		
+		while (resultado.next()) {
+			
+			int codigo = resultado.getInt("codigo");
+			String nome = resultado.getString("nome");
+			
+			pessoas.add(new Pessoa(codigo, nome));
+		}
+		
+			for(Pessoa p: pessoas){
+			System.out.println(p.getCodigo()+ " ➔ " + p.getNome());
+		}
+		
+		stmt.close();
+		conexao.close();	
+	}
+}
