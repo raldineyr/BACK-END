7989f46624650ad608f7c8d91dabcb1a2962dd06
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-11-03 09:06:01 -0300

    Exercício 05 - Persistência obter usuário

---
+++ Linguagem Java/Cod3rcursos/JPA/controle/ObterUsuarios.java
@@ -0,0 +1,43 @@
+package JPA.controle;
+
+import java.util.List;
+
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import br.com.raldineyr.JPA.modelo.Usuario;
+
+public class ObterUsuarios {
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("coder-cursos-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		/*
+		 * String jpql = "SELECT u FROM Usuario u";
+		 * 
+		 * TypedQuery<Usuario> query = em.createQuery(jpql, Usuario.class);
+		 * query.setMaxResults(5);
+		 * 
+		 * List<Usuario> usuarios = query.getResultList();
+		 * 
+		 */
+		
+		// Outra forma de realizar consultas: 
+		
+		List<Usuario> usuarios = em .createQuery("SELECT u FROM Usuario u", Usuario.class)
+		.setMaxResults(5)
+		.getResultList();
+
+		for (Usuario usuario : usuarios) {
+			System.out.println("ID: " + usuario.getId() + " E-mail: " + usuario.getEmail());
+		}
+
+		em.close();
+		emf.close();
+	}
+
+}
