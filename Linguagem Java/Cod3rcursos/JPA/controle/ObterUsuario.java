b22a23fa79871c149ba4d77ffe7a59cb374eb59d
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-11-05 08:19:19 -0300

    Exercício 05 - Obter (um) usuário.

---
+++ Linguagem Java/Cod3rcursos/JPA/controle/ObterUsuario.java
@@ -0,0 +1,24 @@
+package JPA.controle;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import br.com.raldineyr.JPA.modelo.Usuario;
+
+public class ObterUsuario {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("coder-cursos-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		Usuario usuario = em.find(Usuario.class,4L);
+		System.out.println(usuario.getNome());
+		
+		em.close();
+		emf.close();
+	}
+
+}
