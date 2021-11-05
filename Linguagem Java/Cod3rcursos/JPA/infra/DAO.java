8b62db4e7408627b66c91be9da8bb084d1f027b9
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-11-05 08:35:49 -0300

    Exercício 01 - DAO:  Data Access Object

---
+++ Linguagem Java/Cod3rcursos/JPA/infra/DAO.java
@@ -0,0 +1,86 @@
+package br.com.raldineyr.JPA.infra;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+public class DAO<E> {
+
+	private static EntityManagerFactory emf;
+	private EntityManager em;
+	private Class<E> classe;
+
+	static {
+		try {
+			emf = Persistence.createEntityManagerFactory("coder-cursos-jpa");
+		} catch (Exception e) {
+
+		}
+	}
+
+	public DAO(Class<E> classe) {
+
+		this.classe = classe;
+		em = emf.createEntityManager();
+	}
+
+	public DAO<E> abrirTransacao() {
+
+		em.getTransaction().begin();
+		return this;
+	}
+	
+	public DAO<E> fechar(){
+		em.close();
+		
+		return this;
+	}
+
+	public DAO<E> fecharTransacao() {
+
+		em.getTransaction().commit();
+		return this;
+	}
+
+	public DAO<E> incluir(E entidade) {
+
+		em.persist(entidade);
+		return this;
+	}
+
+	public DAO<E> forcarInclusao(E entidade) {
+
+		return this.abrirTransacao().incluir(entidade).fecharTransacao();
+
+	}
+
+	public E obterPorID(Object id) {
+		return em.find(classe, id);
+	}
+
+	public List<E> obterTodos() {
+
+		return this.obterTodos(5, 0);
+	}
+
+	public List<E> obterTodos(int quantidade, int deslocamento) {
+
+		if (classe == null) {
+			throw new UnsupportedOperationException("Classe nula.");
+		}
+
+		String jpql = "SELECT e FROM " + classe.getName() + " e";
+
+		TypedQuery<E> query = em.createQuery(jpql, classe);
+
+		query.setMaxResults(quantidade);
+
+		query.setFirstResult(deslocamento);
+
+		return query.getResultList();
+	}
+
+}
