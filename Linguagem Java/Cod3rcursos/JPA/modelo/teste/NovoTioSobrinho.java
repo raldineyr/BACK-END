148516f4d33e3fcab04ef4af8b676cdb7a5eaaa1
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-11-11 06:11:09 -0300

    Exercício 03 - Persistência adiconar novo sobrinho

---
+++ Linguagem Java/Cod3rcursos/JPA/modelo/teste/NovoTioSobrinho.java
@@ -0,0 +1,38 @@
+package JPA.modelo.teste;
+
+import JPA.infra.DAO;
+import JPA.modelo.muitospramuitos.Sobrinho;
+import JPA.modelo.muitospramuitos.Tio;
+
+public class NovoTioSobrinho {
+
+	public static void main(String[] args) {
+		
+		Tio tia1 = new Tio("Binha");
+		Tio tio2 = new Tio("Zé");
+		
+		Sobrinho sobrinho1 = new Sobrinho("Titela");
+		Sobrinho sobrinho2 = new Sobrinho("Batatão");
+		
+		tia1.getSobrinhos().add(sobrinho1);
+		sobrinho1.getTios().add(tia1);
+		
+		tia1.getSobrinhos().add(sobrinho2);
+		sobrinho2.getTios().add(tia1);
+		
+		tio2.getSobrinhos().add(sobrinho1);
+		sobrinho2.getTios().add(tio2);
+		
+		tio2.getSobrinhos().add(sobrinho2);
+		sobrinho2.getTios().add(tio2);
+		
+		DAO<Object> dao = new DAO<>(null);
+		dao.abrirTransacao()
+		.incluir(tia1)
+		.incluir(tio2)
+		.incluir(sobrinho1)
+		.incluir(sobrinho2)
+		.fecharTransacao()
+		.fechar();
+	}
+}
