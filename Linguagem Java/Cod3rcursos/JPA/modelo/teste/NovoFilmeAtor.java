1c38d1c32e3cc6c83d19d41b59ef218335452dd2
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-11-11 06:07:26 -0300

    Exercício 01 - Persistência adicionar filme e ator

---
+++ Linguagem Java/Cod3rcursos/JPA/modelo/teste/NovoFilmeAtor.java
@@ -0,0 +1,29 @@
+package JPA.modelo.teste;
+
+import JPA.infra.DAO;
+import JPA.modelo.muitospramuitos.Ator;
+import JPA.modelo.muitospramuitos.Filme;
+
+public class NovoFilmeAtor {
+	public static void main(String[] args) {
+
+		Filme filmeA = new Filme("Black Panther", 10.0);
+		Filme filmeB = new Filme("42", 8.7);
+
+		Ator atorA = new Ator("Chadwick Boseman");
+		Ator atorB = new Ator("Michael B. Jordan");
+		Ator atrizA = new Ator("Lupita Nyong'o");
+		Ator atrizB = new Ator("Danai Gurira");
+		
+
+		filmeA.adicionarAtor(atorA);
+		filmeA.adicionarAtor(atorB);
+		filmeA.adicionarAtor(atrizA);
+		filmeA.adicionarAtor(atrizB);
+		
+		filmeB.adicionarAtor(atorA);
+		
+		DAO <Filme> dao = new DAO<Filme>(null);
+		dao.incluir(filmeB);
+	}
+}
