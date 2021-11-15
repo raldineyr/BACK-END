e1d2918bce03a706375503c6d71034ab8b36ed87
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-11-15 10:13:26 -0300

    Exercício 01 - Persistência relacionamento: N —— N

--- Linguagem Java/Cod3rcursos/JPA/modelo/muitospramuitos/Ator.java
+++ Linguagem Java/Cod3rcursos/JPA/modelo/muitospramuitos/Ator.java
@@ -3,6 +3,7 @@ package JPA.modelo.muitospramuitos;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -19,7 +20,7 @@ public class Ator {
 
 	private String nome;
 
-	@ManyToMany (mappedBy = "atores")
+	@ManyToMany (mappedBy = "atores", cascade = CascadeType.PERSIST)
 	private List<Filme> filmes = new ArrayList<>();
 
 	public Ator() {
