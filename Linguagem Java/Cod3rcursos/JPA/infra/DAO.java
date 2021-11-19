7126c40e5c088ab681d1d3825b438dbab8af57b9
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2021-11-19 20:10:07 -0300

    Exercício 01 - DAO: Data Access Object

--- Linguagem Java/Cod3rcursos/JPA/infra/DAO.java
+++ Linguagem Java/Cod3rcursos/JPA/infra/DAO.java
@@ -86,5 +86,15 @@ public class DAO<E> {
 
 		return query.getResultList();
 	}
+	
+	public List<E> consultar(String nomeConsulta, Object... params) {
+		TypedQuery<E> query = em.createNamedQuery(nomeConsulta, classe);
+		
+		for (int i = 0; i < params.length; i += 2) {
+			query.setParameter(params[i].toString(), params[i + 1]);
+		}
+		
+		return query.getResultList();
+	}
 
 }
