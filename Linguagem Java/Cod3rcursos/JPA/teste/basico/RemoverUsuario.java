d424b1c2878df1511a80e910932e06407e79f260
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2022-01-17 08:37:09 -0300

    Exercício 09 - Remover Usuário

---
+++ Linguagem Java/Cod3rcursos/JPA/teste/basico/RemoverUsuario.java
@@ -0,0 +1,28 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class RemoverUsuario {
+
+	public static void main(String[] args) {
+
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+
+		Usuario usuario = em.find(Usuario.class, 6L);
+
+		if (usuario != null) {
+			
+			em.getTransaction().begin();
+			em.remove(usuario);
+			em.getTransaction().commit();
+		}
+
+		em.close();
+		emf.close();
+	}
+}
