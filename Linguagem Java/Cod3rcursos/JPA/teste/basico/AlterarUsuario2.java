8a480208f0d20f39dfc46350b428f580de54a18f
Raldiney Ribeiro <64384382+raldineyr@users.noreply.github.com>
2022-01-17 08:29:12 -0300

    Exercício 06 - Alterar Usuário 2

---
+++ Linguagem Java/Cod3rcursos/JPA/teste/basico/AlterarUsuario2.java
@@ -0,0 +1,30 @@
+package teste.basico;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import modelo.basico.Usuario;
+
+public class AlterarUsuario2 {
+	
+	public static void main(String[] args) {
+		
+		EntityManagerFactory emf = Persistence
+				.createEntityManagerFactory("exercicios-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		em.getTransaction().begin();
+		
+		Usuario usuario = em.find(Usuario.class, 4L);
+		usuario.setNome("Caio");
+
+		// em.merge(usuario);
+		
+		em.getTransaction().commit();
+		
+		em.close();
+		emf.close();
+	}
+
+}
